55dec144c8e37ccf89cfafb20d434fbd2e17e0d9
admin <admin@DESKTOP-EAG2J71>
2019-11-07 12:57:30 +0530

    this is master file committed

---
+++ com.automation.nutriag/src/test/java/com/automation/nutriag/com/automation/nutriag/test2.java
@@ -0,0 +1,11 @@
+package com.automation.nutriag.com.automation.nutriag;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this is master file committed
+		
+	}
+
+}
